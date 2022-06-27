cc23bb7e98d78f8c3d91e1fb2047e499c5cf02cc
Jeremy <jeremy@je12emy.com>
2022-06-27 16:52:13 -0600

    feat(book): update the project "restaurant"
    Introduce the use keyword.
    
    Book-Chapter: https://doc.rust-lang.org/book/ch07-04-bringing-paths-into-scope-with-the-use-keyword.html#bringing-paths-into-scope-with-the-use-keyword

--- the_book/book_exercises/restaurant/src/lib.rs
+++ the_book/book_exercises/restaurant/src/lib.rs
@@ -44,6 +44,8 @@ mod back_of_house {
 
 fn deliver_order() {}
 
+use crate::front_of_house::hosting;
+
 pub fn eat_at_restaurant() {
     let mut meal = back_of_house::Breakfast::summer("Rye");
     meal.toast = String::from("Wheat");
@@ -51,4 +53,6 @@ pub fn eat_at_restaurant() {
 
     let order1 = back_of_house::Appetizer::Soup;
     let order2 = back_of_house::Appetizer::Salad;
+
+    hosting::add_to_waitlist();
 }
