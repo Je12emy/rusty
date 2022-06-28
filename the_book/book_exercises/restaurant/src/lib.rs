7d6861d84e0ba8847f26ba14c4152bc69cf6c067
Jeremy <jeremy@je12emy.com>
2022-06-28 12:43:32 -0600

    feat(book): update the project: "restaurant"
    Move all modules into their own separate files.
    
    Book-Chapter: https://doc.rust-lang.org/book/ch07-05-separating-modules-into-different-files.html#separating-modules-into-different-files

--- the_book/book_exercises/restaurant/src/lib.rs
+++ the_book/book_exercises/restaurant/src/lib.rs
@@ -1,46 +1,5 @@
-mod front_of_house {
-    pub mod hosting {
-        pub fn add_to_waitlist() {}
-
-        fn seat_at_table() {}
-    }
-
-    mod serving {
-        fn take_order() {}
-
-        fn server_order() {}
-
-        fn take_payment() {}
-    }
-}
-
-mod back_of_house {
-    pub enum Appetizer {
-        Soup,
-        Salad,
-    }
-
-    pub struct Breakfast {
-        pub toast: String,
-        seasonal_fruit: String,
-    }
-
-    impl Breakfast {
-        pub fn summer(toast: &str) -> Breakfast {
-            Breakfast {
-                toast: String::from(toast),
-                seasonal_fruit: String::from("peaches"),
-            }
-        }
-    }
-
-    fn fix_incorrent_order() {
-        cook_order();
-        super::deliver_order();
-    }
-
-    fn cook_order() {}
-}
+mod front_of_house;
+mod back_of_house;
 
 fn deliver_order() {}
 
